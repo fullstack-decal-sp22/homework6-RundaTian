28d5bf9ee818e25ea7a57d327c7589695e0756c7
Runda Tian <tian1998@berkeley.edu>
2022-04-07 17:55:28 -0700

    hw6

--- react-app/src/Components/Post.js
+++ react-app/src/Components/Post.js
@@ -1,9 +1,15 @@
-const Post = ({ title, body }) => {
+
+import Comments from "./Comments";
+import NewComment from "./NewComment";
+
+const Post = ({ title, body, postId, comments }) => {
   return <div style={{ textAlign: 'left', marginBottom: '12px'}}>
     <h3>
       {title}
     </h3>
     <p>{body}</p>
+    <Comments comments={comments} />
+    <NewComment postId={postId} />
   </div>
 }
 
